b89d79abc31014410cf25e69e065aec6c58bd7b1
Chris Baldauf <chrisbaldauf@users.noreply.github.com>
2015-05-23 08:50:27 -0400

    Priority comments

--- lib/vagrant-winrm-syncedfolders/plugin.rb
+++ lib/vagrant-winrm-syncedfolders/plugin.rb
@@ -11,6 +11,12 @@ module VagrantPlugins
       mechanism, most notably that file tranfer is slow for large numbers of files.
       EOF
 
+      # The 6 below represents priority. Choosing a value of 6 puts it right in between
+      # the built in synced folder implementations of SMB (7) and RSync (5), which is 
+      # useful since the SMB implementation has some non-trivial prerequisites and the 
+      # RSync implementation isn't guaranteed to work on Windows natively. This provides
+      # a nice way to fall back to something that is pretty sure to work, with the cost
+      # being slower transfer times.
       synced_folder("winrm", 6) do
         require_relative "synced_folder"
         init!
